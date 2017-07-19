652c3d984a8fe670a2e48817ed148980a661b6a4
Thomas Malt <thomas@malt.no>
2017-07-19 12:44:11 +0200

    Setting up example dotnetcore API

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebAPIApplication.Models;
+
+namespace WebAPIApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+        private readonly ILogger _logger;
+
+        public TodoController(TodoContext context, ILogger<TodoController> logger)
+        {
+            _context = context;
+            _logger = logger;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _logger.LogDebug("Looking at todoitems before add: {0}", _context.TodoItems.Count());
+
+                _context.TodoItems.Add(new TodoItem {Name = "Item1"});
+                _context.SaveChanges();
+
+                _logger.LogDebug("Looking at todoitems after add: {0}", _context.TodoItems.Count());
+            }
+        }
+
+        // GET: api/todo
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        // GET api/todo/id
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(int id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(item);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
