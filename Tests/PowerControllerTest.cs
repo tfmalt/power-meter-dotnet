a0e28857277413ef264b71229666609f1563780a
Thomas Malt <thomas.malt@knowit.no>
2017-07-19 16:59:41 +0200

    adding unit tests

---
+++ Tests/PowerControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PowerMeterApi.Tests
+{
+    public class PowerControllerTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(1, 5);
+        }
+    }
+}
