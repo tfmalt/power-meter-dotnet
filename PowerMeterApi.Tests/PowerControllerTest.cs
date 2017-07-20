4c7e967c418433420a2aca0b9a762e71a3c545a8
Thomas Malt <thomas.malt@knowit.no>
2017-07-20 14:12:13 +0200

    Getting tests to work

--- PowerMeterApi.Tests/PowerControllerTest.cs
+++ PowerMeterApi.Tests/PowerControllerTest.cs
@@ -2,22 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Castle.Core.Logging;
 using Xunit;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using PowerMeterApi;
+using PowerMeterApi.Controllers;
+using PowerMeterApi.Models;
+using Microsoft.Extensions.Logging;
 
 namespace PowerMeterApi.Tests
 {
     public class PowerControllerTest
     {
         [Fact]
-        public void PassingTest()
+        public void GetAll_ReturnsListofTodoItems()
         {
-            Assert.Equal(4, 4);
-        }
+            var optionsBuilder = new DbContextOptionsBuilder<TodoContext>();
+            optionsBuilder.UseInMemoryDatabase();
+            var context = new TodoContext(optionsBuilder.Options);
+            //private DbSet<TodoItem> todoitems =
+            //{
+            //    new TodoItem {Id = 1, Name = "Write a working test", IsComplete = false},
+            //    new TodoItem {Id = 2, Name = "Start using xunit", IsComplete = true}
+            //};
+            
+            var mockLogger = new Mock<ILogger<PowerController>>();
+            var power = new PowerController(context, mockLogger.Object);
+            var result = power.GetAll();
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(1, 5);
+            Assert.IsType<List<TodoItem>>(result);
+            Assert.Equal(2, result.Count());
         }
     }
 }
