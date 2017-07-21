6bf8146594ba9319cbb351817d215b4ba2abe66b
Thomas Malt <thomas@malt.no>
2017-07-21 21:05:35 +0200

    Added an object to describe meter total result

---
+++ PowerMeterApi/Models/MeterTotal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PowerMeterApi.Models
+{
+    public class MeterTotal
+    {
+        public Double Value { get; set; }
+        public string Description { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
