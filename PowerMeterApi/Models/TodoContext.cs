0fad3da2c68e3ca432a76b4aaf848811410ef022
Thomas Malt <thomas@malt.no>
2017-07-21 21:05:07 +0200

    syntax minor

--- PowerMeterApi/Models/TodoContext.cs
+++ PowerMeterApi/Models/TodoContext.cs
@@ -9,7 +9,8 @@ namespace PowerMeterApi.Models
 {
     public class TodoContext : DbContext
     {
-        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        public TodoContext(DbContextOptions<TodoContext> options) 
+            : base(options)
         {
             
         }
