a6c92a7ebf36c1482321de0b9d6b0d8a57f5a974
Thomas Malt <thomas@malt.no>
2017-07-21 21:06:11 +0200

    started to rewrite controller to add real power meter functionality

--- PowerMeterApi/Controllers/PowerController.cs
+++ PowerMeterApi/Controllers/PowerController.cs
@@ -5,105 +5,125 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PowerMeterApi.Models;
+using StackExchange.Redis;
 
 namespace PowerMeterApi.Controllers
 {
     [Route("power")]
     public class PowerController : Controller
     {
-        private readonly TodoContext _context;
+        // private readonly TodoContext _context;
         private readonly ILogger _logger;
+        private ConnectionMultiplexer _redis;
 
-        public PowerController(TodoContext context, ILogger<PowerController> logger)
+        public PowerController(ILogger<PowerController> logger)
         {
-            _context = context;
+            _redis = CreateAndReturnRedisConnection();
+
+            // _context = context;
             _logger = logger;
+ 
+            //if (_context.TodoItems.Count() == 0)
+            //{
+            //    _logger.LogDebug("Looking at todoitems before add: {0}", _context.TodoItems.Count());
 
-            if (_context.TodoItems.Count() == 0)
-            {
-                _logger.LogDebug("Looking at todoitems before add: {0}", _context.TodoItems.Count());
+            //    _context.TodoItems.Add(new TodoItem {Name = "Item1"});
+            //    _context.SaveChanges();
 
-                _context.TodoItems.Add(new TodoItem {Name = "Item1"});
-                _context.SaveChanges();
-
-                _logger.LogDebug("Looking at todoitems after add: {0}", _context.TodoItems.Count());
-            }
+            //    _logger.LogDebug("Looking at todoitems after add: {0}", _context.TodoItems.Count());
+            //}
         }
 
-        // GET: api/todo
-        [HttpGet("todo")]
-        public IEnumerable<TodoItem> GetAll()
+        private ConnectionMultiplexer CreateAndReturnRedisConnection() {
+            var redis = ConnectionMultiplexer.Connect(
+                "duro.malt.no:6379,password=g3yHBIdiGPBsoGgPdb6rkEJa"
+            );
+
+            return redis;
+        }
+
+        [HttpGet("meter/total")]
+        public IActionResult GetMeterTotal()
         {
-            return _context.TodoItems.ToList();
+            var db = _redis.GetDatabase();
+
+            var result = db.StringGet("meterTotal");
+            var data = new MeterTotal {
+                Value = (Double)result,
+                Description = "Current Meter Total",
+                Timestamp = DateTime.Now
+            };
+
+            return new ObjectResult(data); 
         }
+
+    //    [HttpPut("todo/{id}")]
+        //public IActionResult Update(long id, [FromBody] TodoItem item)
+    //    {
+    //        if (item == null || item.Id != id)
+    //        {
+    //            return BadRequest();
+    //        }
+
+    //        var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+    //        if (todo == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        todo.IsComplete = item.IsComplete;
+    //        todo.Name = item.Name;
+
+    //        _context.TodoItems.Update(todo);
+    //        _context.SaveChanges();
+
+    //        return new NoContentResult();
+    //    }
 
         // GET api/todo/id
-        [HttpGet("todo/{id}", Name = "GetTodo")]
-        public IActionResult GetById(int id)
-        {
-            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
-            if (item == null)
-            {
-                return NotFound();
-            }
+    //    [HttpGet("todo/{id}", Name = "GetTodo")]
+    //    public IActionResult GetById(int id)
+    //    {
+    //        var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+    //        if (item == null)
+    //        {
+    //            return NotFound();
+    //        }
 
             
-            var result = new ObjectResult(item);
-            return result;
-        }
+    //        var result = new ObjectResult(item);
+    //        return result;
+    //    }
 
 
-        [HttpPost("todo")]
-        public IActionResult Create([FromBody] TodoItem item)
-        {
-            if (item == null)
-            {
-                return BadRequest();
-            }
+    //    [HttpPost("todo")]
+    //    public IActionResult Create([FromBody] TodoItem item)
+    //    {
+    //        if (item == null)
+    //        {
+    //            return BadRequest();
+    //        }
 
-            _context.TodoItems.Add(item);
-            _context.SaveChanges();
+    //        _context.TodoItems.Add(item);
+    //        _context.SaveChanges();
 
-            return CreatedAtRoute("GetTodo", new {id = item.Id}, item);
-        }
+    //        return CreatedAtRoute("GetTodo", new {id = item.Id}, item);
+    //    }
 
-        [HttpPut("todo/{id}")]
-        public IActionResult Update(long id, [FromBody] TodoItem item)
-        {
-            if (item == null || item.Id != id)
-            {
-                return BadRequest();
-            }
+    //    // DELETE api/values/5
+    //    [HttpDelete("todo/{id}")]
+    //    public IActionResult Delete(long id)
+    //    {
+    //        var todo = _context.TodoItems.First(t => t.Id == id);
+    //        if (todo == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
-            if (todo == null)
-            {
-                return NotFound();
-            }
-
-            todo.IsComplete = item.IsComplete;
-            todo.Name = item.Name;
-
-            _context.TodoItems.Update(todo);
-            _context.SaveChanges();
-
-            return new NoContentResult();
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("todo/{id}")]
-        public IActionResult Delete(long id)
-        {
-            var todo = _context.TodoItems.First(t => t.Id == id);
-            if (todo == null)
-            {
-                return NotFound();
-            }
-
-            _context.TodoItems.Remove(todo);
-            _context.SaveChanges();
-            return new NoContentResult();
-        }
+    //        _context.TodoItems.Remove(todo);
+    //        _context.SaveChanges();
+    //        return new NoContentResult();
+    //    }
     }
     
 }
